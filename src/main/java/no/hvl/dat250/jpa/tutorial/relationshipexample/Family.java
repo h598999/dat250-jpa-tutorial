4effa9e1d33fef59f9d6da591fc2eaf39ba72a44
h598999 <h598999@stud.hvl.no>
2024-09-18 16:33:50 +0200

    Tutorial

---
+++ src/main/java/no/hvl/dat250/jpa/tutorial/relationshipexample/Family.java
@@ -0,0 +1,50 @@
+package no.hvl.dat250.jpa.tutorial.relationshipexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+/**
+ * Family
+ */
+@Entity
+public class Family {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String description;
+
+  @OneToMany(mappedBy = "family")
+  private final List<Person> memebers = new ArrayList<>();
+
+  public Family(){}
+
+  public Family(String description){
+    this.description = description;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<Person> getMemebers() {
+    return memebers;
+  }
+}
---
+++ src/main/java/no/hvl/dat250/jpa/tutorial/relationshipexample/Person.java
@@ -0,0 +1,85 @@
+package no.hvl.dat250.jpa.tutorial.relationshipexample;
+
+import java.beans.Transient;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+/**
+ * Person
+ */
+@Entity
+public class Person {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String firstName;
+  private String lastName;
+
+  @ManyToOne
+  private Family family;
+
+  // @Transient
+  // private String nonsenseField = "";
+
+  @OneToMany
+  private List<Job> jobList = new ArrayList<>();
+
+  public Person(String firstName, String lastName, Family family, List<Job> jobList){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.family = family;
+    this.jobList = jobList;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Family getFamily() {
+    return family;
+  }
+
+  public void setFamily(Family family) {
+    this.family = family;
+  }
+
+  public List<Job> getJobList() {
+    return jobList;
+  }
+
+  public void setJobList(List<Job> jobList) {
+    this.jobList = jobList;
+  }
+
+
+  
+}
